dcdae61b32919f713d8470bf239c390fc1c7b84c
Tate Peñaranda <tate@tpenaranda.com>
2019-10-02 14:54:23 -0300

    Added 'api/contacts' endpoint (routing and ContactController).

---
+++ app/Http/Controllers/API/ContactController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\{Contact, CustomAttribute};
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'data' => 'array',
+            'data.*.email' => 'email|max:191|nullable',
+            'data.*.fb_messenger_id' => 'string|max:191|nullable',
+            'data.*.first_name' => 'string|max:191|nullable',
+            'data.*.last_name' => 'string|max:191|nullable',
+            'data.*.phone' => 'string|max:191|required',
+            'data.*.sticky_phone_number_id' => 'numeric|nullable',
+            'data.*.team_id' => 'numeric|required',
+            'data.*.time_zone' => 'string|max:191|nullable',
+            'data.*.twitter_id' => 'string|max:191|nullable',
+            'data.*.unsubscribed_status' => 'string|max:191|required',
+        ]);
+
+        return collect($request->json('data'))->map(function ($data) {
+            $contact = Contact::create($data);
+
+            $customAttributes = array_except($data, $contact->getFillable());
+            $contact->customAttributes()->createMany(collect($customAttributes)->transform(function ($value, $key) {
+                return compact('key', 'value');
+            }));
+
+            return $contact->load('customAttributes');
+        });
+    }
+}
